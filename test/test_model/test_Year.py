19f3d83cc924f7491a425b7fe59b75962ef6e51e
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-24 21:56:01 -0500

    added test

---
+++ test/test_model/test_Year.py
@@ -0,0 +1,19 @@
+import unittest
+
+from src.leeger.model.Team import Team
+from src.leeger.model.Week import Week
+from src.leeger.model.Year import Year
+
+
+class TestYear(unittest.TestCase):
+
+    def test_league_init(self):
+        week = Week(weekNumber=0, matchups=[])
+        team = Team(ownerId="", name="")
+        year = Year(yearNumber=2000, teams=[team], weeks=[week])
+
+        self.assertEqual(2000, year.yearNumber)
+        self.assertEqual(1, len(year.teams))
+        self.assertEqual(1, len(year.weeks))
+        self.assertEqual(week.id, year.weeks[0].id)
+        self.assertEqual(team.id, year.teams[0].id)
