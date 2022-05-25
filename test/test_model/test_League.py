554edee59dfc4377f63b576edd660dc1f48f2927
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-24 21:56:30 -0500

    updated tests

--- test/test_model/test_League.py
+++ test/test_model/test_League.py
@@ -1,22 +1,14 @@
 import unittest
-from decimal import Decimal
 
 from src.leeger.model.League import League
-from src.leeger.model.Matchup import Matchup
 from src.leeger.model.Owner import Owner
-from src.leeger.model.Team import Team
-from src.leeger.model.Week import Week
 from src.leeger.model.Year import Year
 
 
 class TestLeague(unittest.TestCase):
 
     def test_league_init(self):
-        matchup = Matchup(teamAId="", teamBId="", teamAScore=Decimal(),
-                          teamBScore=Decimal())
-        week = Week(weekNumber=0, matchups=[matchup])
-        team = Team(ownerId="", name="")
-        year = Year(yearNumber=0, teams=[team], weeks=[week])
+        year = Year(yearNumber=0, teams=[], weeks=[])
         owner = Owner(name="")
         league = League(name="leagueName", owners=[owner], years=[year])
 
