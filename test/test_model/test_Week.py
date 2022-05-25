9994e72c00b44dc504e68fa7dab235a2d8b4c982
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-24 21:54:09 -0500

    added test

---
+++ test/test_model/test_Week.py
@@ -0,0 +1,17 @@
+import unittest
+from decimal import Decimal
+
+from src.leeger.model.Matchup import Matchup
+from src.leeger.model.Week import Week
+
+
+class TestWeek(unittest.TestCase):
+
+    def test_week_init(self):
+        matchup = Matchup(teamAId="", teamBId="", teamAScore=Decimal(),
+                          teamBScore=Decimal())
+        week = Week(weekNumber=1, matchups=[matchup])
+
+        self.assertEqual(1, week.weekNumber)
+        self.assertEqual(1, len(week.matchups))
+        self.assertEqual(matchup.id, week.matchups[0].id)
