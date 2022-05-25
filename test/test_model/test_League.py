06027214e2a6f310f40c207afb09c1b174ab8bb8
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-24 21:14:18 -0500

    added test

---
+++ test/test_model/test_League.py
@@ -0,0 +1,29 @@
+import unittest
+from decimal import Decimal
+
+from src.leeger.model.League import League
+from src.leeger.model.Matchup import Matchup
+from src.leeger.model.Owner import Owner
+from src.leeger.model.Team import Team
+from src.leeger.model.Week import Week
+from src.leeger.model.Year import Year
+
+
+class TestLeague(unittest.TestCase):
+
+    def test_league_init(self):
+        matchup = Matchup(teamAId="matchupTeamAId", teamBId="matchupTeamBId", teamAScore=Decimal(1),
+                          teamBScore=Decimal(2))
+        week = Week(weekNumber=1, matchups=[matchup])
+        team = Team(ownerId="teamOwnerId", name="teamName")
+        year = Year(yearNumber=2000, teams=[team], weeks=[week])
+        owner = Owner(name="ownerName")
+        league = League(name="leagueName", owners=[owner], years=[year])
+
+        self.assertIsNotNone(league.id)
+        self.assertIsInstance(league.id, str)
+        self.assertEqual("leagueName", league.name)
+        self.assertEqual(1, len(league.owners))
+        self.assertEqual(1, len(league.years))
+        self.assertEqual(owner.id, league.owners[0].id)
+        self.assertEqual(year.id, league.years[0].id)
