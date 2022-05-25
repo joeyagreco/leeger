c225d9b75807d4040ac7547c4338cbea1618f207
jg <57115019+joeyagreco@users.noreply.github.com>
2022-05-24 21:48:01 -0500

    added test class for owner

---
+++ test/test_model/test_Owner.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.leeger.model.Owner import Owner
+
+
+class TestOwner(unittest.TestCase):
+
+    def test_owner_init(self):
+        owner = Owner(name="name")
+
+        self.assertEqual("name", owner.name)
