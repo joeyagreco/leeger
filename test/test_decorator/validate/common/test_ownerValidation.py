fc992e01f8df2232652f8702aa7840c472802da2
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 15:58:17 -0500

    fixing tests

--- test/test_decorator/validate/common/test_ownerValidation.py
+++ test/test_decorator/validate/common/test_ownerValidation.py
@@ -7,7 +7,7 @@ from src.leeger.model.Owner import Owner
 
 class TestOwnerValidation(unittest.TestCase):
 
-    def test_validateLeague_ownerNameIsntTypeStr_raisesException(self):
+    def test_checkAllTypes_ownerNameIsntTypeStr_raisesException(self):
         with self.assertRaises(InvalidOwnerFormatException) as context:
             ownerValidation.checkAllTypes(Owner(name=None))
         self.assertEqual("Owner name must be type 'str'.", str(context.exception))
