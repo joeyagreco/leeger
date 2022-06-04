8c867115341483cc5635e719973539e9e968f1f1
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 15:57:58 -0500

    fixing tests

--- test/test_decorator/validate/common/test_matchupValidation.py
+++ test/test_decorator/validate/common/test_matchupValidation.py
@@ -7,33 +7,33 @@ from src.leeger.model.Matchup import Matchup
 
 class TestMatchupValidation(unittest.TestCase):
 
-    def test_validateLeague_matchupTeamAIdIsntTypeStr_raisesException(self):
+    def test_checkAllTypes_matchupTeamAIdIsntTypeStr_raisesException(self):
         with self.assertRaises(InvalidMatchupFormatException) as context:
             matchupValidation.checkAllTypes(Matchup(teamAId=None, teamBId="bId", teamAScore=1, teamBScore=2))
         self.assertEqual("Matchup teamAId must be type 'str'.", str(context.exception))
 
-    def test_validateLeague_matchupTeamBIdIsntTypeStr_raisesException(self):
+    def test_checkAllTypes_matchupTeamBIdIsntTypeStr_raisesException(self):
         with self.assertRaises(InvalidMatchupFormatException) as context:
             matchupValidation.checkAllTypes(Matchup(teamAId="aId", teamBId=None, teamAScore=1, teamBScore=2))
         self.assertEqual("Matchup teamBId must be type 'str'.", str(context.exception))
 
-    def test_validateLeague_matchupTeamAScoreIsntTypeFloatOrInt_raisesException(self):
+    def test_checkAllTypes_matchupTeamAScoreIsntTypeFloatOrInt_raisesException(self):
         with self.assertRaises(InvalidMatchupFormatException) as context:
             matchupValidation.checkAllTypes(Matchup(teamAId="aId", teamBId="bId", teamAScore=None, teamBScore=2))
         self.assertEqual("Matchup teamAScore must be type 'float' or 'int'.", str(context.exception))
 
-    def test_validateLeague_matchupTeamBScoreIsntTypeFloatOrInt_raisesException(self):
+    def test_checkAllTypes_matchupTeamBScoreIsntTypeFloatOrInt_raisesException(self):
         with self.assertRaises(InvalidMatchupFormatException) as context:
             matchupValidation.checkAllTypes(Matchup(teamAId="aId", teamBId="bId", teamAScore=1, teamBScore=None))
         self.assertEqual("Matchup teamBScore must be type 'float' or 'int'.", str(context.exception))
 
-    def test_validateLeague_matchupTeamAHasTiebreakerIsntTypeBool_raisesException(self):
+    def test_checkAllTypes_matchupTeamAHasTiebreakerIsntTypeBool_raisesException(self):
         with self.assertRaises(InvalidMatchupFormatException) as context:
             matchupValidation.checkAllTypes(
                 Matchup(teamAId="aId", teamBId="bId", teamAScore=1, teamBScore=2, teamAHasTiebreaker=None))
         self.assertEqual("Matchup teamAHasTiebreaker must be type 'bool'.", str(context.exception))
 
-    def test_validateLeague_matchupTeamBHasTiebreakerIsntTypeBool_raisesException(self):
+    def test_checkAllTypes_matchupTeamBHasTiebreakerIsntTypeBool_raisesException(self):
         with self.assertRaises(InvalidMatchupFormatException) as context:
             matchupValidation.checkAllTypes(
                 Matchup(teamAId="aId", teamBId="bId", teamAScore=1, teamBScore=2, teamBHasTiebreaker=None))
