db8eecba8d829db638b21753ea6d5f90203b916b
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 18:48:24 -0500

    added tests

--- test/test_calculator/test_TeamStats.py
+++ test/test_calculator/test_TeamStats.py
@@ -28,3 +28,51 @@ class TestTeamStats(unittest.TestCase):
         self.assertEqual(2, len(response.keys()))
         self.assertEqual(0, response[team1.id])
         self.assertEqual(1, response[team2.id])
+
+    def test_getWins_onlyPostSeasonIsTrue(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3])
+
+        response = TeamStats.getWins(year, onlyPostSeason=True)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(0, response[team1.id])
+        self.assertEqual(2, response[team2.id])
+
+    def test_getWins_onlyRegualarSeasonIsTrue(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3])
+
+        response = TeamStats.getWins(year, onlyRegularSeason=True)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(0, response[team1.id])
+        self.assertEqual(2, response[team2.id])
