56fa997c8c8738fcfd312628f8a6a18fccc32315
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 18:43:15 -0500

    added calculation and base test

--- src/leeger/calculator/TeamStats.py
+++ src/leeger/calculator/TeamStats.py
@@ -1,5 +1,6 @@
 from src.leeger.decorator.validate.validators import validateYear
 from src.leeger.model.Year import Year
+from src.leeger.util.YearNavigator import YearNavigator
 
 
 class TeamStats:
@@ -19,5 +20,25 @@ class TeamStats:
         """
         onlyPostSeason = kwargs.pop("onlyPostSeason", False)  # only include post season wins
         onlyRegularSeason = kwargs.pop("onlyRegularSeason", False)  # only include regular season wins
-        weekNumberStart = kwargs.pop("weekNumberStart", year.weeks[0])  # week to start the calculations at (inclusive)
-        weekNumberEnd = kwargs.pop("weekNumberEnd", year.weeks[-1])  # week to end the calculations at (inclusive)
+        weekNumberStart = kwargs.pop("weekNumberStart",
+                                     year.weeks[0].weekNumber)  # week to start the calculations at (inclusive)
+        weekNumberEnd = kwargs.pop("weekNumberEnd",
+                                   year.weeks[-1].weekNumber)  # week to end the calculations at (inclusive)
+
+        teamIdAndWins = dict()
+        for teamId in YearNavigator.getAllTeamIds(year):
+            teamIdAndWins[teamId] = 0
+
+        for i in range(weekNumberStart - 1, weekNumberEnd):
+            week = year.weeks[i]
+            if (week.isPlayoffWeek and not onlyRegularSeason) or (not week.isPlayoffWeek and not onlyPostSeason):
+                for matchup in week.matchups:
+                    # team A won
+                    if (matchup.teamAScore > matchup.teamBScore) or (
+                            matchup.teamAScore == matchup.teamBScore and matchup.teamAHasTiebreaker):
+                        teamIdAndWins[matchup.teamAId] += 1
+                    # team B won
+                    elif (matchup.teamBScore > matchup.teamAScore) or (
+                            matchup.teamAScore == matchup.teamBScore and matchup.teamBHasTiebreaker):
+                        teamIdAndWins[matchup.teamBId] += 1
+        return teamIdAndWins
---
+++ test/test_calculator/test_TeamStats.py
@@ -0,0 +1,30 @@
+import unittest
+
+from src.leeger.calculator.TeamStats import TeamStats
+from src.leeger.model.Matchup import Matchup
+from src.leeger.model.Owner import Owner
+from src.leeger.model.Team import Team
+from src.leeger.model.Week import Week
+from src.leeger.model.Year import Year
+
+
+class TestTeamStats(unittest.TestCase):
+    def test_getWins_happyPath(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1])
+
+        response = TeamStats.getWins(year)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(0, response[team1.id])
+        self.assertEqual(1, response[team2.id])
