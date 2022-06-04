04362dcd380a79eec0bb5c3eb851657bd0022d7f
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 13:58:46 -0500

    getWins()

--- src/leeger/calculator/TeamStats.py
+++ src/leeger/calculator/TeamStats.py
@@ -1,5 +1,6 @@
 from src.leeger.decorator.validateLeague import validateLeague
 from src.leeger.model.League import League
+from src.leeger.util.LeagueNavigator import LeagueNavigator
 
 
 class TeamStats:
@@ -17,7 +18,8 @@ class TeamStats:
             ...
             }
         """
+        year = LeagueNavigator.getYearByYearNumber(league, yearNumber, validateLeague=False)
         onlyPostSeason = kwargs.pop("onlyPostSeason", False)  # only include post season wins
         onlyRegularSeason = kwargs.pop("onlyRegularSeason", False)  # only include regular season wins
-        weekNumberStart = kwargs.pop("weekNumberStart", None)  # week to start the calculations at (inclusive)
-        weekNumberEnd = kwargs.pop("weekNumberEnd", None)  # week to end the calculations at (inclusive)
+        weekNumberStart = kwargs.pop("weekNumberStart", year.weeks[0])  # week to start the calculations at (inclusive)
+        weekNumberEnd = kwargs.pop("weekNumberEnd", year.weeks[-1])  # week to end the calculations at (inclusive)
