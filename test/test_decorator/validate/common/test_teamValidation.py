14455dd591e9b52b1a16ba712a698a3ec49d6399
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 15:55:22 -0500

    fixing tests

--- test/test_decorator/validate/common/test_teamValidation.py
+++ test/test_decorator/validate/common/test_teamValidation.py
@@ -1,34 +1,18 @@
 import unittest
 
-from src.leeger.decorator.validate.validators import validateLeague
+from src.leeger.decorator.validate.common import teamValidation
 from src.leeger.exception.InvalidTeamFormatException import InvalidTeamFormatException
-from src.leeger.model.League import League
-from src.leeger.model.Owner import Owner
 from src.leeger.model.Team import Team
-from src.leeger.model.Year import Year
 
 
 class TestYearValidation(unittest.TestCase):
 
-    @validateLeague
-    def dummyFunction(self, league: League, **kwargs):
-        """
-        This is used to represent any function that can be wrapped by @validateLeague.
-        """
-        ...
-
     def test_validateLeague_teamOwnerIdIsntTypeStr_raisesException(self):
-        owner = Owner(name="TEST")
-        team = Team(ownerId=None, name="team")
-        year = Year(yearNumber=2000, teams=[team], weeks=list())
         with self.assertRaises(InvalidTeamFormatException) as context:
-            self.dummyFunction(League(name="TEST", owners=[owner], years=[year]))
+            teamValidation.checkAllTypes(Team(ownerId=None, name="team"))
         self.assertEqual("Team owner ID must be type 'str'.", str(context.exception))
 
     def test_validateLeague_teamNameIsntTypeStr_raisesException(self):
-        owner = Owner(name="TEST")
-        team = Team(ownerId="id", name=None)
-        year = Year(yearNumber=2000, teams=[team], weeks=list())
         with self.assertRaises(InvalidTeamFormatException) as context:
-            self.dummyFunction(League(name="TEST", owners=[owner], years=[year]))
+            teamValidation.checkAllTypes(Team(ownerId="id", name=None))
         self.assertEqual("Team name must be type 'str'.", str(context.exception))
