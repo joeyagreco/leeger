fdba52363613c606bb05c266b2f2352f1c3b25b3
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 12:35:21 -0500

    buffed happy path test

--- test/test_decorator/test_validateLeague.py
+++ test/test_decorator/test_validateLeague.py
@@ -28,22 +28,39 @@ class TestValidateLeague(unittest.TestCase):
         owner1 = Owner(name="1")
         owner2 = Owner(name="2")
 
-        team1 = Team(ownerId=owner1.id, name="1")
-        team2 = Team(ownerId=owner2.id, name="2")
+        a_team1 = Team(ownerId=owner1.id, name="1")
+        a_team2 = Team(ownerId=owner2.id, name="2")
 
-        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
-        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
-        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=1, teamAHasTiebreaker=True)
-        matchup4 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        a_matchup1 = Matchup(teamAId=a_team1.id, teamBId=a_team2.id, teamAScore=1, teamBScore=2)
+        a_matchup2 = Matchup(teamAId=a_team1.id, teamBId=a_team2.id, teamAScore=1, teamBScore=2)
+        a_matchup3 = Matchup(teamAId=a_team1.id, teamBId=a_team2.id, teamAScore=1, teamBScore=1,
+                             teamAHasTiebreaker=True)
+        a_matchup4 = Matchup(teamAId=a_team1.id, teamBId=a_team2.id, teamAScore=1, teamBScore=2)
 
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-        week4 = Week(weekNumber=4, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup4])
+        a_week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[a_matchup1])
+        a_week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[a_matchup2])
+        a_week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[a_matchup3])
+        a_week4 = Week(weekNumber=4, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[a_matchup4])
 
-        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3, week4])
+        a_year = Year(yearNumber=2000, teams=[a_team1, a_team2], weeks=[a_week1, a_week2, a_week3, a_week4])
 
-        self.dummyFunction(League(name="TEST", owners=[owner1, owner2], years=[year]))
+        b_team1 = Team(ownerId=owner1.id, name="1")
+        b_team2 = Team(ownerId=owner2.id, name="2")
+
+        b_matchup1 = Matchup(teamAId=b_team1.id, teamBId=b_team2.id, teamAScore=1, teamBScore=2)
+        b_matchup2 = Matchup(teamAId=b_team1.id, teamBId=b_team2.id, teamAScore=1, teamBScore=2)
+        b_matchup3 = Matchup(teamAId=b_team1.id, teamBId=b_team2.id, teamAScore=1, teamBScore=1,
+                             teamAHasTiebreaker=True)
+        b_matchup4 = Matchup(teamAId=b_team1.id, teamBId=b_team2.id, teamAScore=1, teamBScore=2)
+
+        b_week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[b_matchup1])
+        b_week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[b_matchup2])
+        b_week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[b_matchup3])
+        b_week4 = Week(weekNumber=4, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[b_matchup4])
+
+        b_year = Year(yearNumber=2001, teams=[b_team1, b_team2], weeks=[b_week1, b_week2, b_week3, b_week4])
+
+        self.dummyFunction(League(name="TEST", owners=[owner1, owner2], years=[a_year, b_year]))
 
     def test_validateLeague_twoChampionshipWeeksInYear_raisesException(self):
         week1 = Week(weekNumber=1, isPlayoffWeek=True, isChampionshipWeek=True, matchups=list())
