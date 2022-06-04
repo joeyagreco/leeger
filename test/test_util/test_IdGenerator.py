af11c620a4b21a3efdb6b2cdc7e8b189ae77e627
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-04 13:40:08 -0500

    added test class

---
+++ test/test_util/test_IdGenerator.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.leeger.util.IdGenerator import IdGenerator
+
+
+class TestIdGenerator(unittest.TestCase):
+    def test_generateId_happyPath(self):
+        response1 = IdGenerator.generateId()
+        response2 = IdGenerator.generateId()
+        self.assertIsInstance(response1, str)
+        self.assertIsInstance(response2, str)
+        self.assertEqual(32, len(response1))
+        self.assertEqual(32, len(response2))
+        self.assertNotEqual(response1, response2)
