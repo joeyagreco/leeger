3a594d9ebb08637c9a1246469d0531686bb70586
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-05 15:07:47 -0500

    added tests

--- test/test_calculator/parent/test_YearCalculator.py
+++ test/test_calculator/parent/test_YearCalculator.py
@@ -128,3 +128,61 @@ class TestYearCalculator(unittest.TestCase):
         with self.assertRaises(InvalidFilterException) as context:
             YearCalculator.getFilters(year, weekNumberEnd=None)
         self.assertEqual("'weekNumberEnd' must be type 'int'", str(context.exception))
+
+    def test_getFilters_onlyPostSeasonAndOnlyRegularSeasonAreTrue_raisesException(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2])
+
+        with self.assertRaises(InvalidFilterException) as context:
+            YearCalculator.getFilters(year, onlyPostSeason=True, onlyRegularSeason=True)
+        self.assertEqual("'onlyPostSeason' and 'onlyRegularSeason' cannot both be True.", str(context.exception))
+
+    def test_getFilters_weekNumberStartLessThanOne_raisesException(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2])
+
+        with self.assertRaises(InvalidFilterException) as context:
+            YearCalculator.getFilters(year, weekNumberStart=0)
+        self.assertEqual("'weekNumberStart' cannot be less than 1.", str(context.exception))
+
+    def test_getFilters_weekNumberEndGreaterThanNumberOfWeeks_raisesException(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2])
+
+        with self.assertRaises(InvalidFilterException) as context:
+            YearCalculator.getFilters(year, weekNumberEnd=3)
+        self.assertEqual("'weekNumberEnd' cannot be greater than the number of weeks in the year.",
+                         str(context.exception))
