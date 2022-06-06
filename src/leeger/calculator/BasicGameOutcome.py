b4de7b60518b07e85d0545ea76e5c4d2c0e34fb4
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-05 21:13:26 -0500

    added win percentage calculation and tests

--- src/leeger/calculator/BasicGameOutcome.py
+++ src/leeger/calculator/BasicGameOutcome.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from src.leeger.calculator.parent.YearCalculator import YearCalculator
 from src.leeger.decorator.validate.validators import validateYear
 from src.leeger.model.Year import Year
@@ -108,3 +110,38 @@ class BasicGameOutcome(YearCalculator):
                         teamIdAndTies[matchup.teamAId] += 1
                         teamIdAndTies[matchup.teamBId] += 1
         return teamIdAndTies
+
+    @classmethod
+    @validateYear
+    def getWinPercentage(cls, year: Year, **kwargs) -> dict[str, Decimal]:
+        """
+        Returns the win percentage for each team in the given Year.
+        Win percentage will be represented as a non-rounded decimal.
+        Example:
+            33% win percentage -> Decimal("0.3333333333333333333333333333")
+            100% win percentage -> Decimal("1.0")
+
+        Example response:
+            {
+            "someTeamId": 0.85,
+            "someOtherTeamId": .1156,
+            "yetAnotherTeamId": 0.72,
+            ...
+            }
+        """
+        cls.loadFilters(year, validateYear=False, **kwargs)
+
+        teamIdAndWinPercentage = dict()
+        teamIdAndWins = BasicGameOutcome.getWins(year, **kwargs)
+        teamIdAndLosses = BasicGameOutcome.getLosses(year, **kwargs)
+        teamIdAndTies = BasicGameOutcome.getTies(year, **kwargs)
+
+        for teamId in YearNavigator.getAllTeamIds(year):
+            numberOfWins = teamIdAndWins[teamId]
+            numberOfLosses = teamIdAndLosses[teamId]
+            numberOfTies = teamIdAndTies[teamId]
+            numberOfGamesPlayed = numberOfWins + numberOfLosses + numberOfTies
+            teamIdAndWinPercentage[teamId] = (Decimal(numberOfWins) + (Decimal(0.5) * Decimal(numberOfTies))) / Decimal(
+                numberOfGamesPlayed)
+
+        return teamIdAndWinPercentage
--- test/test_calculator/test_BasicGameOutcome.py
+++ test/test_calculator/test_BasicGameOutcome.py
@@ -1,4 +1,5 @@
 import unittest
+from decimal import Decimal
 
 from src.leeger.calculator.BasicGameOutcome import BasicGameOutcome
 from src.leeger.model.Matchup import Matchup
@@ -434,3 +435,151 @@ class TestBasicGameOutcome(unittest.TestCase):
         self.assertEqual(2, len(response.keys()))
         self.assertEqual(1, response[team1.id])
         self.assertEqual(1, response[team2.id])
+
+    def test_getWinPercentage_happyPath(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=2, teamBScore=1)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup4 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=1)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup3])
+        week4 = Week(weekNumber=4, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup4])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3, week4])
+
+        response = BasicGameOutcome.getWinPercentage(year)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Decimal("0.375"), response[team1.id])
+        self.assertEqual(Decimal("0.625"), response[team2.id])
+
+    def test_getWinPercentage_onlyPostSeasonIsTrue(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=2, teamBScore=1)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3])
+
+        response = BasicGameOutcome.getWinPercentage(year, onlyPostSeason=True)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Decimal("0.5"), response[team1.id])
+        self.assertEqual(Decimal("0.5"), response[team2.id])
+
+    def test_getWinPercentage_onlyRegualarSeasonIsTrue(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=2, teamBScore=1)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3])
+
+        response = BasicGameOutcome.getWinPercentage(year, onlyRegularSeason=True)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Decimal("0.0"), response[team1.id])
+        self.assertEqual(Decimal("1.0"), response[team2.id])
+
+    def test_getWinPercentage_weekNumberStartGiven(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=2, teamBScore=1)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3])
+
+        response = BasicGameOutcome.getWinPercentage(year, weekNumberStart=2)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Decimal("0.5"), response[team1.id])
+        self.assertEqual(Decimal("0.5"), response[team2.id])
+
+    def test_getWinPercentage_weekNumberEndGiven(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=2, teamBScore=1)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3])
+
+        response = BasicGameOutcome.getWinPercentage(year, weekNumberEnd=2)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Decimal("0.0"), response[team1.id])
+        self.assertEqual(Decimal("1.0"), response[team2.id])
+
+    def test_getWinPercentage_weekNumberStartGivenAndWeekNumberEndGiven(self):
+        owner1 = Owner(name="1")
+        owner2 = Owner(name="2")
+
+        team1 = Team(ownerId=owner1.id, name="1")
+        team2 = Team(ownerId=owner2.id, name="2")
+
+        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup3 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=2, teamBScore=1)
+        matchup4 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
+        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
+        week4 = Week(weekNumber=4, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup4])
+
+        year = Year(yearNumber=2000, teams=[team1, team2], weeks=[week1, week2, week3, week4])
+
+        response = BasicGameOutcome.getWinPercentage(year, weekNumberStart=2, weekNumberEnd=3)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Decimal("0.5"), response[team1.id])
+        self.assertEqual(Decimal("0.5"), response[team2.id])
