c61a0ec4546bd94d9b9bd01f0533bd039655183a
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-09 18:17:10 -0500

    added validator and tests

--- src/leeger/decorator/validate/validators.py
+++ src/leeger/decorator/validate/validators.py
@@ -1,7 +1,8 @@
 from typing import Callable
 
-from src.leeger.decorator.validate.common import leagueValidation, yearValidation
+from src.leeger.decorator.validate.common import leagueValidation, yearValidation, weekValidation
 from src.leeger.model.League import League
+from src.leeger.model.Week import Week
 from src.leeger.model.Year import Year
 
 
@@ -54,6 +55,33 @@ def validateYear(function: Callable) -> Callable:
         if year is None:
             raise ValueError("No valid Year argument given to validate.")
         yearValidation.runAllChecks(year)
+        return function(*args, **kwargs)
+
+    return wrapFunction
+
+
+def validateWeek(function: Callable) -> Callable:
+    """
+    It is expected that any function decorated with this will follow these rules:
+        - Have a Week object as a parameter
+    This decorator will take the first Week parameter found and use it for all validation
+    If a function that is decorated with this has the kwarg "validateWeek" set to False, this validation will not be run.
+
+    The purpose of this decorator is to do some initial checks on the Week object to validate that it is correctly formatted.
+    """
+
+    def wrapFunction(*args, **kwargs):
+        # check if we should run this method
+        if "validateWeek" in kwargs and not kwargs["validateWeek"]:
+            return function(*args, **kwargs)
+        week = None
+        for arg in args:
+            if type(arg) == Week:
+                week = arg
+                break
+        if week is None:
+            raise ValueError("No valid Week argument given to validate.")
+        weekValidation.runAllChecks(week)
         return function(*args, **kwargs)
 
     return wrapFunction
--- test/test_decorator/test_validators.py
+++ test/test_decorator/test_validators.py
@@ -1,22 +1,54 @@
 import unittest
 
-from src.leeger.decorator.validate.validators import validateLeague
+from src.leeger.decorator.validate.validators import validateLeague, validateYear, validateWeek
 from src.leeger.model.League import League
+from src.leeger.model.Week import Week
+from src.leeger.model.Year import Year
 
 
 class TestValidators(unittest.TestCase):
 
     @validateLeague
-    def dummyFunction(self, league: League, **kwargs):
+    def dummyLeagueFunction(self, league: League, **kwargs):
         """
         This is used to represent any function that can be wrapped by @validateLeague.
         """
         ...
 
+    @validateYear
+    def dummyYearFunction(self, year: Year, **kwargs):
+        """
+        This is used to represent any function that can be wrapped by @validateYear.
+        """
+        ...
+
+    @validateWeek
+    def dummyWeekFunction(self, week: Week, **kwargs):
+        """
+        This is used to represent any function that can be wrapped by @validateWeek.
+        """
+        ...
+
     def test_validateLeague_noLeagueParameterGiven_raisesException(self):
         with self.assertRaises(ValueError) as context:
-            self.dummyFunction(None)
+            self.dummyLeagueFunction(None)
         self.assertEqual("No valid League argument given to validate.", str(context.exception))
 
     def test_validateLeague_validateLeagueKwargIsFalse_doesntRunValidation(self):
-        self.dummyFunction(None, validateLeague=False)
+        self.dummyLeagueFunction(None, validateLeague=False)
+
+    def test_validateYear_noYearParameterGiven_raisesException(self):
+        with self.assertRaises(ValueError) as context:
+            self.dummyYearFunction(None)
+        self.assertEqual("No valid Year argument given to validate.", str(context.exception))
+
+    def test_validateYear_validateYearKwargIsFalse_doesntRunValidation(self):
+        self.dummyYearFunction(None, validateYear=False)
+
+    def test_validateWeek_noWeekParameterGiven_raisesException(self):
+        with self.assertRaises(ValueError) as context:
+            self.dummyWeekFunction(None)
+        self.assertEqual("No valid Week argument given to validate.", str(context.exception))
+
+    def test_validateWeek_validateWeekKwargIsFalse_doesntRunValidation(self):
+        self.dummyWeekFunction(None, validateWeek=False)
