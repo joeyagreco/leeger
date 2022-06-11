4dfcf928fb41b107ee29cad792438c22a3728861
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-10 23:57:30 -0500

    added test

--- test/test_calculator/test_AdvancedGameOutcome.py
+++ test/test_calculator/test_AdvancedGameOutcome.py
@@ -726,3 +726,32 @@ class TestAdvancedGameOutcome(unittest.TestCase):
         self.assertEqual(Decimal("0.7"), response[teams[3].id])
         self.assertEqual(Decimal("0.7"), response[teams[4].id])
         self.assertEqual(Decimal("1"), response[teams[5].id])
+
+    def test_getAWALPerGame_teamsDontPlayEveryWeek(self):
+        owners, teams = getNDefaultOwnersAndTeams(6)
+
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=teams[2].id, teamBId=teams[3].id, teamAScore=3, teamBScore=4)
+        matchup3 = Matchup(teamAId=teams[4].id, teamBId=teams[5].id, teamAScore=4, teamBScore=5)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False,
+                     matchups=[matchup1, matchup2, matchup3])
+
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[5].id, teamAScore=2, teamBScore=1)
+
+        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=True,
+                     matchups=[matchup1])
+
+        year = Year(yearNumber=2000, teams=[teams[0], teams[1], teams[2], teams[3], teams[4], teams[5]],
+                    weeks=[week1, week2])
+
+        response = AdvancedGameOutcome.getAWALPerGame(year)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(6, len(response.keys()))
+        self.assertEqual(Decimal("0.5"), response[teams[0].id])
+        self.assertEqual(Decimal("0.2"), response[teams[1].id])
+        self.assertEqual(Decimal("0.4"), response[teams[2].id])
+        self.assertEqual(Decimal("0.7"), response[teams[3].id])
+        self.assertEqual(Decimal("0.7"), response[teams[4].id])
+        self.assertEqual(Decimal("0.5"), response[teams[5].id])
