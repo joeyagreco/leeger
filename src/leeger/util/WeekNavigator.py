1f414b25b3d2e84fe318b0f31223363c10b99439
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-12 20:45:56 -0500

    added week navigator function and test

--- src/leeger/util/WeekNavigator.py
+++ src/leeger/util/WeekNavigator.py
@@ -26,3 +26,23 @@ class WeekNavigator:
             teamIdAndScores[matchup.teamAId] = matchup.teamAScore
             teamIdAndScores[matchup.teamBId] = matchup.teamBScore
         return teamIdAndScores
+
+    @staticmethod
+    @validateWeek
+    def getTeamIdsAndOpponentScores(week: Week, **kwargs) -> dict[str, float | int]:
+        """
+        Returns all scores for each team's opponent in the given Week.
+
+        Example response:
+            {
+            "someTeamId": 110,
+            "someOtherTeamId": 101.2,
+            "yetAnotherTeamId": 88.4,
+            ...
+            }
+        """
+        teamIdAndScores = dict()
+        for matchup in week.matchups:
+            teamIdAndScores[matchup.teamAId] = matchup.teamBScore
+            teamIdAndScores[matchup.teamBId] = matchup.teamAScore
+        return teamIdAndScores
--- test/test_util/test_WeekNavigator.py
+++ test/test_util/test_WeekNavigator.py
@@ -1,21 +1,16 @@
 import unittest
 
 from src.leeger.model.Matchup import Matchup
-from src.leeger.model.Owner import Owner
-from src.leeger.model.Team import Team
 from src.leeger.model.Week import Week
 from src.leeger.util.WeekNavigator import WeekNavigator
+from test.helper.prototypes import getNDefaultOwnersAndTeams
 
 
 class TestWeekNavigator(unittest.TestCase):
     def test_getTeamIdsAndScores_happyPath(self):
-        owner1 = Owner(name="1")
-        owner2 = Owner(name="2")
+        owners, teams = getNDefaultOwnersAndTeams(2)
 
-        team1 = Team(ownerId=owner1.id, name="1")
-        team2 = Team(ownerId=owner2.id, name="2")
-
-        matchup1 = Matchup(teamAId=team1.id, teamBId=team2.id, teamAScore=1, teamBScore=2)
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=2)
 
         week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
 
@@ -23,5 +18,19 @@ class TestWeekNavigator(unittest.TestCase):
 
         self.assertIsInstance(response, dict)
         self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1, response[team1.id])
-        self.assertEqual(2, response[team2.id])
+        self.assertEqual(1, response[teams[0].id])
+        self.assertEqual(2, response[teams[1].id])
+
+    def test_getTeamIdsAndOpponentScores_happyPath(self):
+        owners, teams = getNDefaultOwnersAndTeams(2)
+
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=2)
+
+        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
+
+        response = WeekNavigator.getTeamIdsAndOpponentScores(week1)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(2, response[teams[0].id])
+        self.assertEqual(1, response[teams[1].id])
