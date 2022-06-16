d6d212d447476723e2bafffac7e15be5c1a4c1a5
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-16 17:35:49 -0500

    moved plus/minus calculations/tests into their own classes

---
+++ src/leeger/calculator/PlusMinusCalculator.py
@@ -0,0 +1,41 @@
+from src.leeger.calculator.ScoringCalculator import ScoringCalculator
+from src.leeger.calculator.parent.YearCalculator import YearCalculator
+from src.leeger.decorator.validate.validators import validateYear
+from src.leeger.model.Year import Year
+from src.leeger.util.Deci import Deci
+from src.leeger.util.YearNavigator import YearNavigator
+
+
+class PlusMinusCalculator(YearCalculator):
+    """
+    Used to calculate all plus/minuses.
+    """
+
+    @classmethod
+    @validateYear
+    def getPlusMinus(cls, year: Year, **kwargs) -> dict[str, Deci]:
+        """
+        Plus/Minus (+/-) is used to show the net score differential for a team in Year.
+        Plus/Minus = ΣA - ΣB
+        WHERE:
+        A = All scores by a team in a Year
+        B = All scores against a team in a Year
+        Returns the Plus/Minus for each team in the given Year.
+
+        Example response:
+            {
+            "someTeamId": Deci("10.7"),
+            "someOtherTeamId": Deci("-11.2"),
+            "yetAnotherTeamId": Deci("34.1"),
+            ...
+            }
+        """
+        cls.loadFilters(year, validateYear=False, **kwargs)
+
+        teamIdAndPlusMinus = dict()
+        teamIdAndPointsScored = ScoringCalculator.getPointsScored(year, **kwargs)
+        teamIdAndOpponentPointsScored = ScoringCalculator.getOpponentPointsScored(year, **kwargs)
+        for teamId in YearNavigator.getAllTeamIds(year):
+            teamIdAndPlusMinus[teamId] = teamIdAndPointsScored[teamId] - teamIdAndOpponentPointsScored[teamId]
+
+        return teamIdAndPlusMinus
