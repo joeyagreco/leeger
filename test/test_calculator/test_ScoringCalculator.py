bc9cdc4b219bf66f22922c17f16a38073e5fb96e
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-16 17:28:30 -0500

    moved single score calculations/tests into their own classes + fixed a bugged test

--- test/test_calculator/test_ScoringCalculator.py
+++ test/test_calculator/test_ScoringCalculator.py
@@ -497,7 +497,6 @@ class TestScoringCalculator(unittest.TestCase):
         self.assertEqual(Deci("2.55"), response[teams[0].id])
         self.assertEqual(Deci("1.25"), response[teams[1].id])
 
-
     def test_getPlusMinus_happyPath(self):
         owners, teams = getNDefaultOwnersAndTeams(2)
 
@@ -518,7 +517,7 @@ class TestScoringCalculator(unittest.TestCase):
         self.assertEqual(Deci("-3.9"), response[teams[0].id])
         self.assertEqual(Deci("3.9"), response[teams[1].id])
 
-    def test_getPointsScored_onlyPostSeasonIsTrue(self):
+    def test_getPlusMinus_onlyPostSeasonIsTrue(self):
         owners, teams = getNDefaultOwnersAndTeams(2)
 
         matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
@@ -531,12 +530,12 @@ class TestScoringCalculator(unittest.TestCase):
 
         year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
 
-        response = ScoringCalculator.getPointsScored(year, onlyPostSeason=True)
+        response = ScoringCalculator.getPlusMinus(year, onlyPostSeason=True)
 
         self.assertIsInstance(response, dict)
         self.assertEqual(2, len(response.keys()))
-        self.assertEqual(Deci("2.5"), response[teams[0].id])
-        self.assertEqual(Deci("5.1"), response[teams[1].id])
+        self.assertEqual(Deci("-2.6"), response[teams[0].id])
+        self.assertEqual(Deci("2.6"), response[teams[1].id])
 
     def test_getPlusMinus_onlyRegularSeasonIsTrue(self):
         owners, teams = getNDefaultOwnersAndTeams(2)
@@ -619,250 +618,6 @@ class TestScoringCalculator(unittest.TestCase):
         self.assertEqual(2, len(response.keys()))
         self.assertEqual(Deci("-2.6"), response[teams[0].id])
         self.assertEqual(Deci("2.6"), response[teams[1].id])
-
-    def test_getMaxScore_happyPath(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=3)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=3)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.9)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMaxScore(year)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.3, response[teams[0].id])
-        self.assertEqual(3, response[teams[1].id])
-
-    def test_getMaxScore_onlyPostSeasonIsTrue(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=5, teamBScore=4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=3)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMaxScore(year, onlyPostSeason=True)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.3, response[teams[0].id])
-        self.assertEqual(3, response[teams[1].id])
-
-    def test_getMaxScore_onlyRegularSeasonIsTrue(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=5, teamBScore=6)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMaxScore(year, onlyRegularSeason=True)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.2, response[teams[0].id])
-        self.assertEqual(5, response[teams[1].id])
-
-    def test_getMaxScore_weekNumberStartGiven(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=3, teamBScore=6)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMaxScore(year, weekNumberStart=2)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.3, response[teams[0].id])
-        self.assertEqual(2.6, response[teams[1].id])
-
-    def test_getMaxScore_weekNumberEndGiven(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=3, teamBScore=3)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMaxScore(year, weekNumberEnd=2)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.2, response[teams[0].id])
-        self.assertEqual(2.5, response[teams[1].id])
-
-    def test_getMaxScore_weekNumberStartGivenAndWeekNumberEndGiven(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6)
-        matchup4 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.4, teamBScore=2.7)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-        week4 = Week(weekNumber=4, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup4])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3, week4])
-
-        response = ScoringCalculator.getMaxScore(year, weekNumberStart=2, weekNumberEnd=3)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.3, response[teams[0].id])
-        self.assertEqual(2.6, response[teams[1].id])
-
-    def test_getMinScore_happyPath(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=1)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=3)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.9)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMinScore(year)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.1, response[teams[0].id])
-        self.assertEqual(1, response[teams[1].id])
-
-    def test_getMinScore_onlyPostSeasonIsTrue(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=1)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=True, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMinScore(year, onlyPostSeason=True)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.2, response[teams[0].id])
-        self.assertEqual(2, response[teams[1].id])
-
-    def test_getMinScore_onlyRegularSeasonIsTrue(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=1)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMinScore(year, onlyRegularSeason=True)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.1, response[teams[0].id])
-        self.assertEqual(2, response[teams[1].id])
-
-    def test_getMinScore_weekNumberStartGiven(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=1)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMinScore(year, weekNumberStart=2)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.2, response[teams[0].id])
-        self.assertEqual(2.5, response[teams[1].id])
-
-    def test_getMinScore_weekNumberEndGiven(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=2)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
-
-        response = ScoringCalculator.getMinScore(year, weekNumberEnd=2)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.1, response[teams[0].id])
-        self.assertEqual(2.4, response[teams[1].id])
-
-    def test_getMinScore_weekNumberStartGivenAndWeekNumberEndGiven(self):
-        owners, teams = getNDefaultOwnersAndTeams(2)
-
-        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
-        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5)
-        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6)
-        matchup4 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.4, teamBScore=2.7)
-
-        week1 = Week(weekNumber=1, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup1])
-        week2 = Week(weekNumber=2, isPlayoffWeek=False, isChampionshipWeek=False, matchups=[matchup2])
-        week3 = Week(weekNumber=3, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup3])
-        week4 = Week(weekNumber=4, isPlayoffWeek=True, isChampionshipWeek=False, matchups=[matchup4])
-
-        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3, week4])
-
-        response = ScoringCalculator.getMinScore(year, weekNumberStart=2, weekNumberEnd=3)
-
-        self.assertIsInstance(response, dict)
-        self.assertEqual(2, len(response.keys()))
-        self.assertEqual(1.2, response[teams[0].id])
-        self.assertEqual(2.5, response[teams[1].id])
 
     def test_getScoringStandardDeviation_happyPath(self):
         owners, teams = getNDefaultOwnersAndTeams(2)
