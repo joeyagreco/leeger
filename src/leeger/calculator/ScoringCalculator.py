1cb1c72bfce201d1d146aa8fbe3370640b0c2a33
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-16 17:22:21 -0500

    moved scoring share calculations/tests into their own classes

--- src/leeger/calculator/ScoringCalculator.py
+++ src/leeger/calculator/ScoringCalculator.py
@@ -127,67 +127,6 @@ class ScoringCalculator(YearCalculator):
 
     @classmethod
     @validateYear
-    def getScoringShare(cls, year: Year, **kwargs) -> dict[str, Deci]:
-        """
-        Scoring Share is used to show what percentage of league scoring a team was responsible for.
-        Scoring Share = ((ΣA) / (ΣB)) * 100
-        WHERE:
-        A = All scores by a Team in a Year
-        B = All scores by all Teams in a Year
-
-        Returns the Scoring Share for each team in the given Year.
-
-        Example response:
-            {
-            "someTeamId": Deci("10.7"),
-            "someOtherTeamId": Deci("14.2"),
-            "yetAnotherTeamId": Deci("12.1"),
-            ...
-            }
-        """
-        cls.loadFilters(year, validateYear=False, **kwargs)
-
-        teamIdAndPointsScored = cls.getPointsScored(year, **kwargs)
-        totalPointsScoredInYear = sum(teamIdAndPointsScored.values())
-        teamIdAndScoringShare = dict()
-        for teamId in YearNavigator.getAllTeamIds(year):
-            teamIdAndScoringShare[teamId] = (teamIdAndPointsScored[teamId] / totalPointsScoredInYear) * Deci(100)
-
-        return teamIdAndScoringShare
-
-    @classmethod
-    @validateYear
-    def getOpponentScoringShare(cls, year: Year, **kwargs) -> dict[str, Deci]:
-        """
-        Opponent Scoring Share is used to show what percentage of league scoring a team's opponent was responsible for.
-        Opponent Scoring Share = ((ΣA) / (ΣB)) * 100
-        WHERE:
-        A = All scores by a Team's opponent in a Year
-        B = All scores by all Teams in a Year
-
-        Returns the Opponent Scoring Share for each team in the given Year.
-
-        Example response:
-            {
-            "someTeamId": Deci("10.7"),
-            "someOtherTeamId": Deci("14.2"),
-            "yetAnotherTeamId": Deci("12.1"),
-            ...
-            }
-        """
-        cls.loadFilters(year, validateYear=False, **kwargs)
-
-        teamIdAndOpponentPointsScored = cls.getOpponentPointsScored(year, **kwargs)
-        totalPointsScoredInYear = sum(teamIdAndOpponentPointsScored.values())
-        teamIdAndOpponentScoringShare = dict()
-        for teamId in YearNavigator.getAllTeamIds(year):
-            teamIdAndOpponentScoringShare[teamId] = (teamIdAndOpponentPointsScored[
-                                                         teamId] / totalPointsScoredInYear) * Deci(100)
-
-        return teamIdAndOpponentScoringShare
-
-    @classmethod
-    @validateYear
     def getPlusMinus(cls, year: Year, **kwargs) -> dict[str, Deci]:
         """
         Plus/Minus (+/-) is used to show the net score differential for a team in Year.
---
+++ src/leeger/calculator/ScoringShareCalculator.py
@@ -0,0 +1,73 @@
+from src.leeger.calculator.ScoringCalculator import ScoringCalculator
+from src.leeger.calculator.parent.YearCalculator import YearCalculator
+from src.leeger.decorator.validate.validators import validateYear
+from src.leeger.model.Year import Year
+from src.leeger.util.Deci import Deci
+from src.leeger.util.YearNavigator import YearNavigator
+
+
+class ScoringShareCalculator(YearCalculator):
+    """
+    Used to calculate all scoring shares.
+    """
+
+    @classmethod
+    @validateYear
+    def getScoringShare(cls, year: Year, **kwargs) -> dict[str, Deci]:
+        """
+        Scoring Share is used to show what percentage of league scoring a team was responsible for.
+        Scoring Share = ((ΣA) / (ΣB)) * 100
+        WHERE:
+        A = All scores by a Team in a Year
+        B = All scores by all Teams in a Year
+
+        Returns the Scoring Share for each team in the given Year.
+
+        Example response:
+            {
+            "someTeamId": Deci("10.7"),
+            "someOtherTeamId": Deci("14.2"),
+            "yetAnotherTeamId": Deci("12.1"),
+            ...
+            }
+        """
+        cls.loadFilters(year, validateYear=False, **kwargs)
+
+        teamIdAndPointsScored = ScoringCalculator.getPointsScored(year, **kwargs)
+        totalPointsScoredInYear = sum(teamIdAndPointsScored.values())
+        teamIdAndScoringShare = dict()
+        for teamId in YearNavigator.getAllTeamIds(year):
+            teamIdAndScoringShare[teamId] = (teamIdAndPointsScored[teamId] / totalPointsScoredInYear) * Deci(100)
+
+        return teamIdAndScoringShare
+
+    @classmethod
+    @validateYear
+    def getOpponentScoringShare(cls, year: Year, **kwargs) -> dict[str, Deci]:
+        """
+        Opponent Scoring Share is used to show what percentage of league scoring a team's opponent was responsible for.
+        Opponent Scoring Share = ((ΣA) / (ΣB)) * 100
+        WHERE:
+        A = All scores by a Team's opponent in a Year
+        B = All scores by all Teams in a Year
+
+        Returns the Opponent Scoring Share for each team in the given Year.
+
+        Example response:
+            {
+            "someTeamId": Deci("10.7"),
+            "someOtherTeamId": Deci("14.2"),
+            "yetAnotherTeamId": Deci("12.1"),
+            ...
+            }
+        """
+        cls.loadFilters(year, validateYear=False, **kwargs)
+
+        teamIdAndOpponentPointsScored = ScoringCalculator.getOpponentPointsScored(year, **kwargs)
+        totalPointsScoredInYear = sum(teamIdAndOpponentPointsScored.values())
+        teamIdAndOpponentScoringShare = dict()
+        for teamId in YearNavigator.getAllTeamIds(year):
+            teamIdAndOpponentScoringShare[teamId] = (teamIdAndOpponentPointsScored[
+                                                         teamId] / totalPointsScoredInYear) * Deci(100)
+
+        return teamIdAndOpponentScoringShare
