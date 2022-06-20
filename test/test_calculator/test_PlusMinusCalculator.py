c0fadf610d372dff6db67df3b3c3d2c3c1a7c7bc
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-19 19:39:34 -0500

    added test

--- test/test_calculator/test_PlusMinusCalculator.py
+++ test/test_calculator/test_PlusMinusCalculator.py
@@ -76,6 +76,28 @@ class TestPlusMinusCalculator(unittest.TestCase):
         self.assertEqual(Deci("-2.6"), response[teams[0].id])
         self.assertEqual(Deci("2.6"), response[teams[1].id])
 
+    def test_getPlusMinus_onlyChampionshipIsTrue(self):
+        owners, teams = getNDefaultOwnersAndTeams(2)
+
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.1, teamBScore=2.4)
+        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.2, teamBScore=2.5,
+                           matchupType=MatchupType.PLAYOFF)
+        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1.3, teamBScore=2.6,
+                           matchupType=MatchupType.CHAMPIONSHIP)
+
+        week1 = Week(weekNumber=1, matchups=[matchup1])
+        week2 = Week(weekNumber=2, matchups=[matchup2])
+        week3 = Week(weekNumber=3, matchups=[matchup3])
+
+        year = Year(yearNumber=2000, teams=[teams[0], teams[1]], weeks=[week1, week2, week3])
+
+        response = PlusMinusCalculator.getPlusMinus(year, onlyChampionship=True)
+
+        self.assertIsInstance(response, dict)
+        self.assertEqual(2, len(response.keys()))
+        self.assertEqual(Deci("-1.3"), response[teams[0].id])
+        self.assertEqual(Deci("1.3"), response[teams[1].id])
+
     def test_getPlusMinus_weekNumberStartGiven(self):
         owners, teams = getNDefaultOwnersAndTeams(2)
 
