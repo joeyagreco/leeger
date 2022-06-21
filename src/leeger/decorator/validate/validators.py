36f9170d029b5a513f30e805ba2c11f48733070a
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-21 17:44:03 -0500

    added matchup validator annotation

--- src/leeger/decorator/validate/validators.py
+++ src/leeger/decorator/validate/validators.py
@@ -1,7 +1,8 @@
 from typing import Callable
 
-from src.leeger.decorator.validate.common import leagueValidation, yearValidation, weekValidation
+from src.leeger.decorator.validate.common import leagueValidation, yearValidation, weekValidation, matchupValidation
 from src.leeger.model.League import League
+from src.leeger.model.Matchup import Matchup
 from src.leeger.model.Week import Week
 from src.leeger.model.Year import Year
 
@@ -82,6 +83,33 @@ def validateWeek(function: Callable) -> Callable:
         if week is None:
             raise ValueError("No valid Week argument given to validate.")
         weekValidation.runAllChecks(week)
+        return function(*args, **kwargs)
+
+    return wrapFunction
+
+
+def validateMatchup(function: Callable) -> Callable:
+    """
+    It is expected that any function decorated with this will follow these rules:
+        - Have a Matchup object as a parameter
+    This decorator will take the first Week parameter found and use it for all validation
+    If a function that is decorated with this has the kwarg "validateMatchup" set to False, this validation will not be run.
+
+    The purpose of this decorator is to do some initial checks on the Matchup object to validate that it is correctly formatted.
+    """
+
+    def wrapFunction(*args, **kwargs):
+        # check if we should run this method
+        if "validateMatchup" in kwargs and not kwargs["validateMatchup"]:
+            return function(*args, **kwargs)
+        matchup = None
+        for arg in args:
+            if type(arg) == Matchup:
+                matchup = arg
+                break
+        if matchup is None:
+            raise ValueError("No valid Matchup argument given to validate.")
+        matchupValidation.runAllChecks(matchup)
         return function(*args, **kwargs)
 
     return wrapFunction
