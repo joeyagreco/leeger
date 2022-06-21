c98249c25c59b97752cf4392feababdddabd6685
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-21 17:43:23 -0500

    added matchup navigator method and tests

---
+++ test/test_util/test_MatchupNavigator.py
@@ -0,0 +1,40 @@
+import unittest
+
+from src.leeger.model.Matchup import Matchup
+from src.leeger.util.MatchupNavigator import MatchupNavigator
+from test.helper.prototypes import getNDefaultOwnersAndTeams
+
+
+class TestMatchupNavigator(unittest.TestCase):
+    def test_getTeamIdOfMatchupWinner_happyPath(self):
+        owners, teams = getNDefaultOwnersAndTeams(6)
+
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=2)
+        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=2, teamBScore=1)
+        matchup3 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=1)
+
+        response1 = MatchupNavigator.getTeamIdOfMatchupWinner(matchup1)
+        response2 = MatchupNavigator.getTeamIdOfMatchupWinner(matchup2)
+        response3 = MatchupNavigator.getTeamIdOfMatchupWinner(matchup3)
+
+        self.assertIsInstance(response1, str)
+        self.assertIsInstance(response2, str)
+        self.assertEqual(teams[1].id, response1)
+        self.assertEqual(teams[0].id, response2)
+        self.assertIsNone(response3)
+
+    def test_getTeamIdOfMatchupWinner_tiebreakersGiven(self):
+        owners, teams = getNDefaultOwnersAndTeams(6)
+
+        matchup1 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=1,
+                           teamAHasTiebreaker=True)
+        matchup2 = Matchup(teamAId=teams[0].id, teamBId=teams[1].id, teamAScore=1, teamBScore=1,
+                           teamBHasTiebreaker=True)
+
+        response1 = MatchupNavigator.getTeamIdOfMatchupWinner(matchup1)
+        response2 = MatchupNavigator.getTeamIdOfMatchupWinner(matchup2)
+
+        self.assertIsInstance(response1, str)
+        self.assertIsInstance(response2, str)
+        self.assertEqual(teams[0].id, response1)
+        self.assertEqual(teams[1].id, response2)
