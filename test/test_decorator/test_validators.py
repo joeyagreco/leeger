b05bf1653dfc3af40d837110e33d3393c5faa070
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-21 18:29:05 -0500

    added tests

--- test/test_decorator/test_validators.py
+++ test/test_decorator/test_validators.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.leeger.decorator.validate.validators import validateLeague, validateYear, validateWeek
+from src.leeger.decorator.validate.validators import validateLeague, validateYear, validateWeek, validateMatchup
 from src.leeger.model.League import League
 from src.leeger.model.Week import Week
 from src.leeger.model.Year import Year
@@ -29,6 +29,15 @@ class TestValidators(unittest.TestCase):
         """
         ...
 
+    @validateMatchup
+    def dummyMatchupFunction(self, week: Week, **kwargs):
+        """
+        This is used to represent any function that can be wrapped by @validateMatchup.
+        """
+        ...
+
+    # Validate League
+
     def test_validateLeague_noLeagueParameterGiven_raisesException(self):
         with self.assertRaises(ValueError) as context:
             self.dummyLeagueFunction(None)
@@ -36,6 +45,8 @@ class TestValidators(unittest.TestCase):
 
     def test_validateLeague_validateLeagueKwargIsFalse_doesntRunValidation(self):
         self.dummyLeagueFunction(None, validateLeague=False)
+
+    # Validate Year
 
     def test_validateYear_noYearParameterGiven_raisesException(self):
         with self.assertRaises(ValueError) as context:
@@ -45,10 +56,22 @@ class TestValidators(unittest.TestCase):
     def test_validateYear_validateYearKwargIsFalse_doesntRunValidation(self):
         self.dummyYearFunction(None, validateYear=False)
 
+    # Validate Week
+
     def test_validateWeek_noWeekParameterGiven_raisesException(self):
         with self.assertRaises(ValueError) as context:
             self.dummyWeekFunction(None)
         self.assertEqual("No valid Week argument given to validate.", str(context.exception))
 
     def test_validateWeek_validateWeekKwargIsFalse_doesntRunValidation(self):
+        self.dummyWeekFunction(None, validateWeek=False)
+
+    # Validate Matchup
+
+    def test_validateMatchup_noMatchupParameterGiven_raisesException(self):
+        with self.assertRaises(ValueError) as context:
+            self.dummyMatchupFunction(None)
+        self.assertEqual("No valid Matchup argument given to validate.", str(context.exception))
+
+    def test_validateMatchup_validateMatchupKwargIsFalse_doesntRunValidation(self):
         self.dummyWeekFunction(None, validateWeek=False)
