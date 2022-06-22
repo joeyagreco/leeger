26ad8ce74faad8f6218718a5a61a06c894f556ce
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-21 19:22:15 -0500

    changed from Deci to int

--- src/leeger/util/YearNavigator.py
+++ src/leeger/util/YearNavigator.py
@@ -1,7 +1,6 @@
 from src.leeger.decorator.validate.validators import validateYear
 from src.leeger.model.Year import Year
 from src.leeger.model.YearFilters import YearFilters
-from src.leeger.util.Deci import Deci
 
 
 class YearNavigator:
@@ -16,15 +15,15 @@ class YearNavigator:
 
     @classmethod
     @validateYear
-    def getNumberOfGamesPlayed(cls, year: Year, yearFilters: YearFilters) -> dict[str, Deci]:
+    def getNumberOfGamesPlayed(cls, year: Year, yearFilters: YearFilters) -> dict[str, int]:
         """
         Returns the number of games played for each team in the given Year.
 
         Example response:
             {
-            "someTeamId": Deci("4"),
-            "someOtherTeamId": Deci("4"),
-            "yetAnotherTeamId": Deci("5"),
+            "someTeamId": 4,
+            "someOtherTeamId": 6,
+            "yetAnotherTeamId": 11,
             ...
             }
         """
@@ -32,12 +31,12 @@ class YearNavigator:
         teamIdAndNumberOfGamesPlayed = dict()
         allTeamIds = YearNavigator.getAllTeamIds(year)
         for teamId in allTeamIds:
-            teamIdAndNumberOfGamesPlayed[teamId] = Deci(0)
+            teamIdAndNumberOfGamesPlayed[teamId] = 0
 
         for i in range(yearFilters.weekNumberStart - 1, yearFilters.weekNumberEnd):
             week = year.weeks[i]
             for matchup in week.matchups:
                 if matchup.matchupType in yearFilters.includeMatchupTypes:
-                    teamIdAndNumberOfGamesPlayed[matchup.teamAId] += Deci(1)
-                    teamIdAndNumberOfGamesPlayed[matchup.teamBId] += Deci(1)
+                    teamIdAndNumberOfGamesPlayed[matchup.teamAId] += 1
+                    teamIdAndNumberOfGamesPlayed[matchup.teamBId] += 1
         return teamIdAndNumberOfGamesPlayed
