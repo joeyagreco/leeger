675b3f241f8a99d2f5d80f377485257fd8550a93
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-24 19:12:39 -0500

    added basic test and updated requirements

---
+++ test/test_league_loader/test_ESPNLeagueLoader.py
@@ -0,0 +1,17 @@
+import unittest
+
+from espn_api.requests.espn_requests import ESPNInvalidLeague
+
+from src.leeger.league_loader.ESPNLeagueLoader import ESPNLeagueLoader
+
+
+class TestESPNLeagueLoader(unittest.TestCase):
+    """
+    Currently, there is not a good way to test this class due to an issue addressed here: https://github.com/cwendt94/espn-api/issues/338
+    For now, we can do some very basic tests.
+    """
+
+    def test_loadLeague_intendedFailure(self):
+        with self.assertRaises(ESPNInvalidLeague) as context:
+            ESPNLeagueLoader.loadLeague(0, [2000])  # 0 is a bad league ID
+        self.assertEqual("League 0 does not exist", str(context.exception))
