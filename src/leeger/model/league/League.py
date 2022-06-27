76ed5f497e1ef60a42cdff9fbf40cd16184f947d
jg <57115019+joeyagreco@users.noreply.github.com>
2022-06-26 23:39:18 -0500

    error handling + tests for excel

--- src/leeger/model/league/League.py
+++ src/leeger/model/league/League.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass
 
 from src.leeger.model.abstract.UniqueId import UniqueId
@@ -12,5 +13,7 @@ class League(UniqueId):
     years: list[Year]
 
     def yearsToExcel(self, filePath: str, **kwargs) -> None:
+        if os.path.exists(filePath):
+            raise FileExistsError(f"Cannot create file at path: '{filePath}' because there is already a file there.")
         for year in self.years:
             year.toExcel(filePath, **kwargs)
--- test/test_model/test_league/test_League.py
+++ test/test_model/test_league/test_League.py
@@ -25,7 +25,7 @@ class TestLeague(unittest.TestCase):
         self.assertEqual(owner.id, league.owners[0].id)
         self.assertEqual(year.id, league.years[0].id)
 
-    def test_league_yearsToExcel(self):
+    def test_league_yearsToExcel_happyPath(self):
         owners, teams1 = getNDefaultOwnersAndTeams(2)
         teams1[0].name = "a"
         teams1[1].name = "b"
@@ -56,3 +56,33 @@ class TestLeague(unittest.TestCase):
             self.assertEqual("1999", workbook.sheetnames[0])
             self.assertEqual("2000", workbook.sheetnames[1])
             self.assertEqual("2001", workbook.sheetnames[2])
+
+    def test_league_yearsToExcel_fileAlreadyExists_raisesException(self):
+        owners, teams1 = getNDefaultOwnersAndTeams(2)
+        teams1[0].name = "a"
+        teams1[1].name = "b"
+        matchup1 = Matchup(teamAId=teams1[0].id, teamBId=teams1[1].id, teamAScore=1, teamBScore=2)
+        week1 = Week(weekNumber=1, matchups=[matchup1])
+        year1 = Year(yearNumber=2000, teams=teams1, weeks=[week1])
+
+        teams2 = [Team(ownerId=owners[0].id, name="a2"), Team(ownerId=owners[1].id, name="b2")]
+        matchup2 = Matchup(teamAId=teams2[0].id, teamBId=teams2[1].id, teamAScore=1, teamBScore=2)
+        week2 = Week(weekNumber=1, matchups=[matchup2])
+        year2 = Year(yearNumber=2001, teams=teams2, weeks=[week2])
+
+        teams3 = [Team(ownerId=owners[0].id, name="a3"), Team(ownerId=owners[1].id, name="b3")]
+        matchup3 = Matchup(teamAId=teams3[0].id, teamBId=teams3[1].id, teamAScore=1, teamBScore=2)
+        week3 = Week(weekNumber=1, matchups=[matchup3])
+        year3 = Year(yearNumber=1999, teams=teams3, weeks=[week3])
+
+        league = League(name="", owners=owners, years=[year1, year2, year3])
+
+        with self.assertRaises(FileExistsError) as context:
+            with tempfile.TemporaryDirectory() as tempDir:
+                fullPath = os.path.join(tempDir, "tmp.xlsx")
+                # create a file with this same name/path first
+                with open(fullPath, "w") as f:
+                    ...
+                league.yearsToExcel(fullPath)
+        self.assertEqual(f"Cannot create file at path: '{fullPath}' because there is already a file there.",
+                         str(context.exception))
