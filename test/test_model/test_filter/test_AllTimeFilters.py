3767acbfefb37294eb7b1039e3e443823c774c6a
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-01 14:03:29 -0500

    added tests

---
+++ test/test_model/test_filter/test_AllTimeFilters.py
@@ -0,0 +1,31 @@
+import unittest
+
+from src.leeger.enum.MatchupType import MatchupType
+from src.leeger.model.filter.AllTimeFilters import AllTimeFilters
+
+
+class TestAllTimeFilters(unittest.TestCase):
+    def test_includeMatchupTypes_default(self):
+        allTimeFilters = AllTimeFilters(yearNumberStart=2000, yearNumberEnd=2001, weekNumberStart=1, weekNumberEnd=2,
+                                        onlyChampionship=False, onlyPostSeason=False, onlyRegularSeason=False)
+
+        self.assertEqual([MatchupType.REGULAR_SEASON, MatchupType.PLAYOFF, MatchupType.CHAMPIONSHIP],
+                         allTimeFilters.includeMatchupTypes)
+
+    def test_includeMatchupTypes_onlyChampionshipIsTrue(self):
+        allTimeFilters = AllTimeFilters(yearNumberStart=2000, yearNumberEnd=2001, weekNumberStart=1, weekNumberEnd=2,
+                                        onlyChampionship=True, onlyPostSeason=False, onlyRegularSeason=False)
+
+        self.assertEqual([MatchupType.CHAMPIONSHIP], allTimeFilters.includeMatchupTypes)
+
+    def test_includeMatchupTypes_onlyPostSeasonIsTrue(self):
+        allTimeFilters = AllTimeFilters(yearNumberStart=2000, yearNumberEnd=2001, weekNumberStart=1, weekNumberEnd=2,
+                                        onlyChampionship=False, onlyPostSeason=True, onlyRegularSeason=False)
+
+        self.assertEqual([MatchupType.PLAYOFF, MatchupType.CHAMPIONSHIP], allTimeFilters.includeMatchupTypes)
+
+    def test_includeMatchupTypes_onlyRegularSeasonIsTrue(self):
+        allTimeFilters = AllTimeFilters(yearNumberStart=2000, yearNumberEnd=2001, weekNumberStart=1, weekNumberEnd=2,
+                                        onlyChampionship=False, onlyPostSeason=False, onlyRegularSeason=True)
+
+        self.assertEqual([MatchupType.REGULAR_SEASON], allTimeFilters.includeMatchupTypes)
