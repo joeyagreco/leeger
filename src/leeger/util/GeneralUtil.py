ddbeeb3b3195e65a049544ddd54807f950132c30
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-06 17:10:14 -0500

    added docstring

--- src/leeger/util/GeneralUtil.py
+++ src/leeger/util/GeneralUtil.py
@@ -5,4 +5,7 @@ class GeneralUtil:
 
     @classmethod
     def filter(cls, *, value: Any, list_: list) -> list:
+        """
+        Filters out the given value from the given list.
+        """
         return [v for v in list_ if v is not value]
