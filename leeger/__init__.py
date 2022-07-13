bda237be38684cc980ff54e747580a9f195e9151
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-12 20:37:13 -0500

    fixed init

--- leeger/__init__.py
+++ leeger/__init__.py
@@ -1,4 +1,4 @@
-from league_loader.ESPNLeagueLoader import ESPNLeagueLoader
-from league_loader.YahooLeagueLoader import YahooLeagueLoader
+from .league_loader.ESPNLeagueLoader import ESPNLeagueLoader
+from .league_loader.YahooLeagueLoader import YahooLeagueLoader
 
 __version__ = "0.1.0"
