7ad2681b2c4979ffeaaf95b0bfb841378e85acaf
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-29 14:05:09 -0500

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("LICENSE") as f:
 
 setuptools.setup(
     name="leeger",
-    version="0.5.0",
+    version="0.6.0",
     author="Joey Greco",
     author_email="joeyagreco@gmail.com",
     description="Instant stats for your fantasy football league.",
