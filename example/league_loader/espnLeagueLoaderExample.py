4e0db39aaae77b21db126420bd27b0058a17992e
jg <57115019+joeyagreco@users.noreply.github.com>
2022-07-30 00:01:04 -0500

    cleanup

--- example/league_loader/espnLeagueLoaderExample.py
+++ example/league_loader/espnLeagueLoaderExample.py
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     league.toExcel("C:\\myLeagueStats.xlsx")
 
     # PRIVATE LEAGUE
-    # Get a League object with years 2019 and 2020 for ESPN league with ID: 12345678.
     # You will need the ESPN_S2 and SWID parameters to retrieve a private league.
     # To retrieve these, follow these steps:
     # 1. Visit your main league page (i.e. https://fantasy.espn.com/football/team?leagueId=12345678seasonId=2020)
@@ -18,6 +17,7 @@ if __name__ == "__main__":
     # 3. Open Developer Tools (on Chrome/Firefox, right-click anywhere on the page and select Inspect Element)
     # 4. Go to Storage (for Firefox) or Application (for Chrome) and browse the Cookies available for fantasy.espn.com
     # 5. The values you need are called "SWID" and "ESPN_S2". You can right-click and copy the values from here.
+    # Get a League object with years 2019 and 2020 for ESPN league with ID: 12345678.
     espnS2 = "ABCDEFG1234567"
     swid = "{ABC-DEF-GHI-JKL-MNOP}"
     espnLeagueLoader = ESPNLeagueLoader("12345678", [2019, 2020], espnS2=espnS2, swid=swid)
