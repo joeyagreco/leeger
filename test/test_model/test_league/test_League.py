b6cb2d9dbe139800b011470562207e5956288d2b
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-03 18:11:08 -0500

    added test

--- test/test_model/test_league/test_League.py
+++ test/test_model/test_league/test_League.py
@@ -28,7 +28,7 @@ class TestLeague(unittest.TestCase):
         self.assertEqual(owner.id, league.owners[0].id)
         self.assertEqual(year.id, league.years[0].id)
 
-    def test_league_eq_happyPath(self):
+    def test_league_eq_equal(self):
         # create League 1
         owners_1, teams_1 = getNDefaultOwnersAndTeams(2)
 
@@ -48,6 +48,27 @@ class TestLeague(unittest.TestCase):
         league_2 = League(name="LEAGUE", owners=owners_2, years=[year_2])
 
         self.assertEqual(league_1, league_2)
+
+    def test_league_eq_notEqual(self):
+        # create League 1
+        owners_1, teams_1 = getNDefaultOwnersAndTeams(2)
+
+        matchup_1 = Matchup(teamAId=teams_1[0].id, teamBId=teams_1[1].id, teamAScore=1.1, teamBScore=2.2,
+                            matchupType=MatchupType.REGULAR_SEASON)
+        week_1 = Week(weekNumber=1, matchups=[matchup_1])
+        year_1 = Year(yearNumber=2000, teams=teams_1, weeks=[week_1])
+        league_1 = League(name="LEAGUE 1", owners=owners_1, years=[year_1])
+
+        # create League 2
+        owners_2, teams_2 = getNDefaultOwnersAndTeams(2)
+
+        matchup_2 = Matchup(teamAId=teams_2[0].id, teamBId=teams_2[1].id, teamAScore=1.1, teamBScore=2.2,
+                            matchupType=MatchupType.REGULAR_SEASON)
+        week_2 = Week(weekNumber=1, matchups=[matchup_2])
+        year_2 = Year(yearNumber=2000, teams=teams_2, weeks=[week_2])
+        league_2 = League(name="LEAGUE 2", owners=owners_2, years=[year_2])
+
+        self.assertNotEqual(league_1, league_2)
 
     def test_league_add_happyPath(self):
         # create League 1
