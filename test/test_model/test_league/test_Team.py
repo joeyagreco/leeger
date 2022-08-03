2f9e551c78aef3871059cc1535cd1dffb491f06c
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-03 18:17:54 -0500

    added tests

--- test/test_model/test_league/test_Team.py
+++ test/test_model/test_league/test_Team.py
@@ -9,3 +9,21 @@ class TestTeam(unittest.TestCase):
 
         self.assertEqual("ownerId", team.ownerId)
         self.assertEqual("name", team.name)
+
+    def test_team_eq_equal(self):
+        # create Team 1
+        team_1 = Team(ownerId="1", name="1")
+
+        # create Team 2
+        team_2 = Team(ownerId="1", name="1")
+
+        self.assertEqual(team_1, team_2)
+
+    def test_team_eq_notEqual(self):
+        # create Team 1
+        team_1 = Team(ownerId="1", name="1")
+
+        # create Team 2
+        team_2 = Team(ownerId="2", name="2dif")
+
+        self.assertNotEqual(team_1, team_2)
