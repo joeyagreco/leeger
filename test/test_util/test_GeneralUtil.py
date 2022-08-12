fef233d52f65832c5d292adac9099d202b4849a6
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-12 18:18:43 -0500

    added test

--- test/test_util/test_GeneralUtil.py
+++ test/test_util/test_GeneralUtil.py
@@ -13,3 +13,11 @@ class TestGeneralUtil(unittest.TestCase):
         l = list()
         response = GeneralUtil.filter(value=None, list_=l)
         self.assertEqual([], response)
+
+    def test_warnForUnusedKwargs_happyPath(self):
+        kwargs = {"a": 1, "b": 2}
+        with self.assertLogs() as captured:
+            GeneralUtil.warnForUnusedKwargs(kwargs)
+        self.assertEqual(2, len(captured.records))
+        self.assertEqual("Keyword argument 'a' unused.", captured.records[0].getMessage())
+        self.assertEqual("Keyword argument 'b' unused.", captured.records[1].getMessage())
