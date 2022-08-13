6706a332ad0cf3c7ced5e1bd21db12b82d7d4083
jg <57115019+joeyagreco@users.noreply.github.com>
2022-08-13 01:02:22 -0500

    updated init file and bumped version

---
+++ leeger/validate/__init__.py
@@ -0,0 +1,6 @@
+from .leagueValidation import runAllChecks
+from .matchupValidation import runAllChecks
+from .ownerValidation import runAllChecks
+from .teamValidation import runAllChecks
+from .weekValidation import runAllChecks
+from .yearValidation import runAllChecks
