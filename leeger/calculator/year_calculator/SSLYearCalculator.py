2fdd436dea4946b2fe275930c5efccf54f936d91
jg <57115019+joeyagreco@users.noreply.github.com>
2022-09-19 20:05:30 -0500

    added team score per game for year stats with tests

--- leeger/calculator/year_calculator/SSLYearCalculator.py
+++ leeger/calculator/year_calculator/SSLYearCalculator.py
@@ -142,3 +142,35 @@ class SSLYearCalculator(YearCalculator):
                 teamIdAndTeamLuck[teamId] = teamSuccess - teamScore
 
         return teamIdAndTeamLuck
+
+    @classmethod
+    @validateYear
+    def getTeamScorePerGame(cls, year: Year, **kwargs) -> dict[str, Optional[Deci]]:
+        """
+        Returns the Team Score per game for each team in the given Year.
+        Returns None if any stat used to calculate this is not found in the given range.
+
+        Example response:
+            {
+            "someTeamId": Deci("118.7"),
+            "someOtherTeamId": Deci("112.2"),
+            "yetAnotherTeamId": Deci("79.1"),
+            ...
+            }
+        """
+        from leeger.calculator.year_calculator import TeamSummaryYearCalculator
+        teamIdAndTeamScorePerGame: dict[str, Deci] = dict()
+
+        teamIdsAndTeamScores = SSLYearCalculator.getTeamScore(year, **kwargs)
+        teamIdsAndGamesPlayed = TeamSummaryYearCalculator.getGamesPlayed(year, **kwargs)
+
+        for teamId in YearNavigator.getAllTeamIds(year):
+            teamScore = teamIdsAndTeamScores[teamId]
+            gamesPlayed = teamIdsAndGamesPlayed[teamId]
+
+            if teamScore is not None and gamesPlayed != 0:
+                teamIdAndTeamScorePerGame[teamId] = Deci(teamScore / gamesPlayed)
+            else:
+                teamIdAndTeamScorePerGame[teamId] = None
+
+        return teamIdAndTeamScorePerGame
