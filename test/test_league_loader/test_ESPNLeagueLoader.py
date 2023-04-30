8f56d6df393cdb99c80d6d2116fc6490d1255966
jg <57115019+joeyagreco@users.noreply.github.com>
2023-04-30 13:52:28 -0500

    added base mock ESPNLeagueLoader test

--- test/test_league_loader/test_ESPNLeagueLoader.py
+++ test/test_league_loader/test_ESPNLeagueLoader.py
@@ -1,8 +1,16 @@
 import unittest
+from unittest.mock import patch, Mock
 
 from espn_api.requests.espn_requests import ESPNInvalidLeague
+from leeger.enum.MatchupType import MatchupType
 
 from leeger.league_loader.ESPNLeagueLoader import ESPNLeagueLoader
+from leeger.model.league.League import League
+from leeger.model.league.Matchup import Matchup
+from leeger.model.league.Owner import Owner
+from leeger.model.league.Team import Team
+from leeger.model.league.Week import Week
+from leeger.model.league.Year import Year
 
 
 class TestESPNLeagueLoader(unittest.TestCase):
@@ -27,3 +35,64 @@ class TestESPNLeagueLoader(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             ESPNLeagueLoader("0", [])
         self.assertEqual("No years given to load league with ID '0'.", str(context.exception))
+
+    @patch("espn_api.football.League")
+    def test_load_league(self, mock_league):
+        mock_espn_league = Mock()
+        mock_espn_league.year = 2022
+        mock_espn_league.current_week = 1
+        mock_espn_league.settings.name = "Test League"
+        mock_espn_league.settings.reg_season_count = 12
+        mock_espn_league.teams = [
+            Mock(
+                team_id=1,
+                owner="Owner 1",
+                team_name="Team 1",
+                outcomes=["W"],
+                scores=[100],
+                schedule=[Mock(team_id=2)],
+            ),
+            Mock(
+                team_id=2,
+                owner="Owner 2",
+                team_name="Team 2",
+                outcomes=["L"],
+                scores=[80],
+                schedule=[Mock(team_id=1)],
+            ),
+        ]
+        mock_league.return_value = mock_espn_league
+
+        loader = ESPNLeagueLoader("123", [2022])
+        league = loader.loadLeague()
+
+        # expected league
+        team1 = Team(ownerId=1, name="Team 1")
+        team2 = Team(ownerId=2, name="Team 2")
+
+        expected_league = League(
+            name="Test League",
+            owners=[Owner(name="Owner 1"), Owner(name="Owner 2")],
+            years=[
+                Year(
+                    yearNumber=2022,
+                    teams=[team1, team2],
+                    weeks=[
+                        Week(
+                            weekNumber=1,
+                            matchups=[
+                                Matchup(
+                                    teamAId=team1.id,
+                                    teamBId=team2.id,
+                                    teamAScore=100,
+                                    teamBScore=80,
+                                    matchupType=MatchupType.REGULAR_SEASON,
+                                )
+                            ],
+                        )
+                    ],
+                )
+            ],
+        )
+        print(league)
+        self.assertEqual(league, expected_league)
