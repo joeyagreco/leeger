9c1664555bc0d08dadac68e1c2663e73ac195a84
jg <57115019+joeyagreco@users.noreply.github.com>
2023-05-07 18:34:58 -0500

    stuff

--- example/league_loader/sleeperLeagueLoaderExample.py
+++ example/league_loader/sleeperLeagueLoaderExample.py
@@ -2,6 +2,6 @@ from leeger.league_loader import SleeperLeagueLoader
 from leeger.model.league import League
 
 if __name__ == "__main__":
-    # Get a League object with years 2019 and 2020 for Sleeper league with ID: "12345678".
+    # Get a League object with years 2019 and 2020 for the Sleeper league with the most recent league ID of "12345678".
     sleeperLeagueLoader = SleeperLeagueLoader("12345678", [2019, 2020])
     league: League = sleeperLeagueLoader.loadLeague()
