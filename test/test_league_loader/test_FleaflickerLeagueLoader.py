38ea767ddc81bb7ee1065151aea64712d8168261
jg <57115019+joeyagreco@users.noreply.github.com>
2023-05-14 15:12:04 -0500

    updated test

--- test/test_league_loader/test_FleaflickerLeagueLoader.py
+++ test/test_league_loader/test_FleaflickerLeagueLoader.py
@@ -31,8 +31,15 @@ class TestFleaflickerLeagueLoader(unittest.TestCase):
     ):
         mockTeam1_2022 = {"owners": [{"displayName": "Owner 1"}], "id": 1, "name": "Team 1"}
         mockTeam2_2022 = {"owners": [{"displayName": "Owner 2"}], "id": 2, "name": "Team 2"}
+        mockTeam3_2022 = {"owners": [{"displayName": "Owner 3"}], "id": 3, "name": "Team 3"}
+        mockTeam4_2022 = {"owners": [{"displayName": "Owner 4"}], "id": 4, "name": "Team 4"}
+        mockTeam5_2022 = {"owners": [{"displayName": "Owner 5"}], "id": 5, "name": "Team 5"}
+        mockTeam6_2022 = {"owners": [{"displayName": "Owner 6"}], "id": 6, "name": "Team 6"}
+        mockTeam7_2022 = {"owners": [{"displayName": "Owner 7"}], "id": 7, "name": "Team 7"}
+        mockTeam8_2022 = {"owners": [{"displayName": "Owner 8"}], "id": 8, "name": "Team 8"}
+        
         mockLeagueStandings2022 = {
-            "divisions": [{"teams": [mockTeam1_2022, mockTeam2_2022]}],
+            "divisions": [{"teams": [mockTeam1_2022, mockTeam2_2022, mockTeam3_2022, mockTeam4_2022, mockTeam5_2022, mockTeam6_2022, mockTeam7_2022, mockTeam8_2022]}],
             "league": {"name": "Test League 2022", "id": 123},
             "season": 2022,
         }
@@ -43,11 +50,38 @@ class TestFleaflickerLeagueLoader(unittest.TestCase):
                     "away": mockTeam1_2022,
                     "home": mockTeam2_2022,
                     "awayScore": {"score": {"value": 100}},
+                    "homeScore": {"score": {"value": 100}},
+                    "awayResult": "WIN",
+                    "homeResult": "LOSS",
+                    "isFinalScore": True,
+                },
+                                {
+                    "away": mockTeam3_2022,
+                    "home": mockTeam4_2022,
+                    "awayScore": {"score": {"value": 100}},
                     "homeScore": {"score": {"value": 90}},
                     "awayResult": "WIN",
                     "homeResult": "LOSS",
                     "isFinalScore": True,
-                }
+                },
+                                                {
+                    "away": mockTeam5_2022,
+                    "home": mockTeam6_2022,
+                    "awayScore": {"score": {"value": 100.1}},
+                    "homeScore": {"score": {"value": 90.1}},
+                    "awayResult": "WIN",
+                    "homeResult": "LOSS",
+                    "isFinalScore": True,
+                },
+                                                                {
+                    "away": mockTeam7_2022,
+                    "home": mockTeam8_2022,
+                    "awayScore": {"score": {"value": 100.2}},
+                    "homeScore": {"score": {"value": 90.2}},
+                    "awayResult": "WIN",
+                    "homeResult": "LOSS",
+                    "isFinalScore": True,
+                },
             ]
         }
 
@@ -58,3 +92,92 @@ class TestFleaflickerLeagueLoader(unittest.TestCase):
 
         leagueLoader = FleaflickerLeagueLoader("123", [2022])
         league = leagueLoader.loadLeague()
+        
+        # expected league
+        team1_2022 = Team(ownerId=1, name="Team 1")
+        team2_2022 = Team(ownerId=2, name="Team 2")
+        team3_2022 = Team(ownerId=3, name="Team 3")
+        team4_2022 = Team(ownerId=4, name="Team 4")
+        team5_2022 = Team(ownerId=5, name="Team 5")
+        team6_2022 = Team(ownerId=6, name="Team 6")
+        team7_2022 = Team(ownerId=7, name="Team 7")
+        team8_2022 = Team(ownerId=8, name="Team 8")
+
+        expectedLeague = League(
+            name="Test League 2022",
+            owners=[
+                Owner(name="Owner 1"),
+                Owner(name="Owner 2"),
+                Owner(name="Owner 3"),
+                Owner(name="Owner 4"),
+                Owner(name="Owner 5"),
+                Owner(name="Owner 6"),
+                Owner(name="Owner 7"),
+                Owner(name="Owner 8"),
+            ],
+            years=[
+                Year(
+                    yearNumber=2022,
+                    teams=[
+                        team1_2022,
+                        team2_2022,
+                        team3_2022,
+                        team4_2022,
+                        team5_2022,
+                        team6_2022,
+                        team7_2022,
+                        team8_2022,
+                    ],
+                    weeks=[
+                        Week(
+                            weekNumber=1,
+                            matchups=[
+                                Matchup(
+                                    teamAId=team1_2022.id,
+                                    teamBId=team2_2022.id,
+                                    teamAScore=100,
+                                    teamBScore=100,
+                                    matchupType=MatchupType.REGULAR_SEASON,
+                                    teamAHasTiebreaker=True,
+                                    teamBHasTiebreaker=False,
+                                ),
+                                Matchup(
+                                    teamAId=team3_2022.id,
+                                    teamBId=team4_2022.id,
+                                    teamAScore=100,
+                                    teamBScore=90,
+                                    matchupType=MatchupType.REGULAR_SEASON,
+                                    teamAHasTiebreaker=True,
+                                    teamBHasTiebreaker=False,
+                                ),
+                                Matchup(
+                                    teamAId=team5_2022.id,
+                                    teamBId=team6_2022.id,
+                                    teamAScore=100.1,
+                                    teamBScore=90.1,
+                                    matchupType=MatchupType.REGULAR_SEASON,
+                                    teamAHasTiebreaker=True,
+                                    teamBHasTiebreaker=False,
+                                ),
+                                Matchup(
+                                    teamAId=team7_2022.id,
+                                    teamBId=team8_2022.id,
+                                    teamAScore=100.2,
+                                    teamBScore=90.2,
+                                    matchupType=MatchupType.REGULAR_SEASON,
+                                    teamAHasTiebreaker=True,
+                                    teamBHasTiebreaker=False,
+                                ),
+                            ],
+                        )],
+                    yearSettings=None,
+                ),
+                ],
+        )
+
+        self.assertEqual(league, expectedLeague)
+        # check multiWeekMatchupIds
+        for year in league.years:
+            for week in year.weeks:
+                for matchup in week.matchups:
+                    self.assertIsNone(matchup.multiWeekMatchupId)
