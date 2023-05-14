176074601651264571c9f5d9bbc6a030165b29b8
jg <57115019+joeyagreco@users.noreply.github.com>
2023-05-14 12:41:36 -0500

    updated test

--- test/test_league_loader/test_FleaflickerLeagueLoader.py
+++ test/test_league_loader/test_FleaflickerLeagueLoader.py
@@ -4,7 +4,6 @@ from unittest.mock import Mock
 import copy
 from leeger.enum.MatchupType import MatchupType
 
-from leeger.league_loader import MyFantasyLeagueLeagueLoader
 from leeger.model.league.League import League
 from leeger.model.league.Matchup import Matchup
 from leeger.model.league.Owner import Owner
@@ -19,16 +18,32 @@ class TestFleaflickerLeagueLoader(unittest.TestCase):
     """
     # TODO: Add better tests.
     """
-
     def test_init_leagueIdNotIntConvertable_raisesException(self):
         with self.assertRaises(ValueError) as context:
             FleaflickerLeagueLoader("foo", [])
         self.assertEqual("League ID 'foo' could not be turned into an int.", str(context.exception))
 
     @mock.patch("fleaflicker.api.LeagueInfoAPIClient.LeagueInfoAPIClient.get_league_standings")
-    def test_loadLeague_happyPath_noOwnerNamesAndAliases(self, mockGetLeaguestandings):
+    @mock.patch("fleaflicker.api.ScoringAPIClient.ScoringAPIClient.get_league_scoreboard")
+    def test_loadLeague_happyPath_noOwnerNamesAndAliases(self, mockGetLeagueScoreboard,mockGetLeaguestandings):
+        mockTeam1_2022 = {"owners": [{"displayName": "Owner 1"}], "id": 1, "name": "Team 1"}
+        mockTeam2_2022 = {"owners": [{"displayName": "Owner 2"}], "id": 2, "name": "Team 2"}
         mockLeagueStandings2022 = {
-            "divisions": [{"teams": [{"owners": [{"displayName": "Owner 1"}]}]}]
+            "divisions": [{"teams": [mockTeam1_2022, mockTeam2_2022]}],
+            "league": {"name": "Test League 2022", "id": 123},
+            "season": 2022
+        }
+        
+        mockWeek1_2022 = {
+            "games": [{"away": mockTeam1_2022, "home": mockTeam2_2022, "awayScore": {"score": {"value": 100}}, "homeScore": {"score": {"value": 90}}, "awayResult": "WIN", "homeResult": "LOSS", "isFinalScore": True}]
+        }
+        
+        mockScoreboard2022 = {
+            "eligibleSchedulePeriods": [mockWeek1_2022]
         }
 
         mockGetLeaguestandings.side_effect = [mockLeagueStandings2022]
+        mockGetLeagueScoreboard.side_effect = [mockScoreboard2022, mockWeek1_2022]
+        
+        leagueLoader = FleaflickerLeagueLoader("123", [2022])
+        league = leagueLoader.loadLeague()
