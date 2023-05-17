101feea686ebb5640c1bdab468d0aab1590491bd
jg <57115019+joeyagreco@users.noreply.github.com>
2023-05-16 20:57:13 -0500

    added division model

---
+++ leeger/model/league/Division.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from leeger.model.abstract.UniqueId import UniqueId
+from leeger.util.CustomLogger import CustomLogger
+from leeger.util.GeneralUtil import GeneralUtil
+from leeger.util.JSONDeserializable import JSONDeserializable
+from leeger.util.JSONSerializable import JSONSerializable
+
+
+@dataclass(kw_only=True, eq=False)
+class Division(UniqueId, JSONSerializable, JSONDeserializable):
+    __LOGGER = CustomLogger.getLogger()
+    name: str
+
+    def __eq__(self, otherDivision: Division) -> bool:
+        """
+        Checks if *this* Division is the same as the given Division.
+        Does not check for equality of IDs, just values.
+        """
+        equal = self.name == otherDivision.name
+        if not equal:
+            differences = GeneralUtil.findDifferentFields(
+                self.toJson(),
+                otherDivision.toJson(),
+                parentKey="Division",
+                ignoreKeyNames=["id", "ownerId", "teamAId", "teamBId"],
+            )
+            self.__LOGGER.info(f"Differences: {differences}")
+        return equal
+
+    def toJson(self) -> dict:
+        return {"id": self.id, "name": self.name}
+
+    @staticmethod
+    def fromJson(d: dict) -> Division:
+        division = Division(name=d["name"])
+        division.id = d["id"]
+        return division
