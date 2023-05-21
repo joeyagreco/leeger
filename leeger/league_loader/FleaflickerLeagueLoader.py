1c649c2d1e0c26fec3fed68b7457104922ef8089
jg <57115019+joeyagreco@users.noreply.github.com>
2023-05-20 22:36:44 -0500

    FF divisions for LL. tests failing(thats ok)

--- leeger/league_loader/FleaflickerLeagueLoader.py
+++ leeger/league_loader/FleaflickerLeagueLoader.py
@@ -13,6 +13,7 @@ from leeger.model.league.Team import Team
 from leeger.model.league.Week import Week
 from leeger.model.league.Year import Year
 from leeger.validate import leagueValidation
+from leeger.model.league.Division import Division
 
 
 class FleaflickerLeagueLoader(LeagueLoader):
@@ -37,6 +38,9 @@ class FleaflickerLeagueLoader(LeagueLoader):
 
         self.__fleaflickerTeamIdToOwnerMap: dict[int, Owner] = dict()
         self.__fleaflickerTeamIdToTeamMap: dict[int, Team] = dict()
+        self.__fleaflickerDivisionIdToDivisionMap: dict[
+            int, Division
+        ] = dict()  # holds the division info for ONLY the current year
 
     def __getAllLeagues(self) -> list[dict]:
         # return a list of all leagues
@@ -82,9 +86,22 @@ class FleaflickerLeagueLoader(LeagueLoader):
         return League(name=self._getLeagueName(), owners=owners, years=self._getValidYears(years))
 
     def __buildYear(self, fleaflickerLeague: dict) -> Year:
+        # save division info
+        for fleaflickerDivision in fleaflickerLeague["divisions"]:
+            self.__fleaflickerDivisionIdToDivisionMap[fleaflickerDivision["id"]] = Division(
+                name=fleaflickerDivision["name"]
+            )
         teams = self.__buildTeams(fleaflickerLeague)
         weeks = self.__buildWeeks(fleaflickerLeague)
-        return Year(yearNumber=int(fleaflickerLeague["season"]), teams=teams, weeks=weeks)
+        year = Year(
+            yearNumber=int(fleaflickerLeague["season"]),
+            teams=teams,
+            weeks=weeks,
+            divisions=list(self.__fleaflickerDivisionIdToDivisionMap.values()),
+        )
+        # clear division info
+        self.__fleaflickerDivisionIdToDivisionMap = dict()
+        return year
 
     def __buildWeeks(self, fleaflickerLeague: dict) -> list[Week]:
         weeks = list()
@@ -160,7 +177,11 @@ class FleaflickerLeagueLoader(LeagueLoader):
                 teamName = team["name"]
                 teamId = team["id"]
                 owner = self.__fleaflickerTeamIdToOwnerMap[teamId]
-                team = Team(ownerId=owner.id, name=teamName)
+                team = Team(
+                    ownerId=owner.id,
+                    name=teamName,
+                    divisionId=self.__fleaflickerDivisionIdToDivisionMap[division["id"]].id,
+                )
                 teams.append(team)
                 self.__fleaflickerTeamIdToTeamMap[teamId] = team
         return teams
--- leeger/util/ConfigReader.py
+++ leeger/util/ConfigReader.py
@@ -3,7 +3,6 @@ import os
 from typing import Optional
 
 
-
 class ConfigReader:
     """
     Used to read from .properties files
