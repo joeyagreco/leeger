24859cde24b530d91e033123c5995c42a5fcc20f
jg <57115019+joeyagreco@users.noreply.github.com>
2023-05-27 16:46:21 -0500

    stuff

--- example/league/comparingLeagueObjects.py
+++ example/league/comparingLeagueObjects.py
@@ -4,15 +4,17 @@ from leeger.model.league import League
 
 if __name__ == "__main__":
     # to compare a League model (or any of its child models) you can use .equals()
-    # 1. take any model (let's get a dummy League) for an example
 
+    # 1. take any model (let's get a dummy League) for an example
     espnLeagueLoader = ESPNLeagueLoader("12345678", [2019, 2020])
     league: League = espnLeagueLoader.loadLeague()
 
     # 2. lets compare it
     league.equals(league)  # compares models with all fields
     league.equals(league, ignoreBaseIds=True)  # ignores the base "id" field in each object
-    league.equals(league, ignoreIds=True)  # ignores fields that hold an id but not the base "id"
+    league.equals(
+        league, ignoreIds=True
+    )  # ignores fields that hold an id but not the base "id" fields
     league.equals(
         league, logDifferences=True
     )  # logs all differences IF the equality check is False
