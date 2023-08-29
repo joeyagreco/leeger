53cc03ac1e5330eb78edc5b4ba7757d110ed5ed6
paymitch13 <112431335+paymitch13@users.noreply.github.com>
2023-08-28 23:07:12 -0400

    Update statsToExcelExample.py

--- example/stat/statsToExcelExample.py
+++ example/stat/statsToExcelExample.py
@@ -5,7 +5,7 @@ from leeger.util.excel import leagueToExcel
 if __name__ == "__main__":
     # Get a League object.
     # There are many ways to get a League object, here we will just grab one using the ESPN League Loader.
-    espnLeagueLoader = ESPNLeagueLoader("12345678", [2019, 2020, 2021, 2022])
+    espnLeagueLoader = ESPNLeagueLoader("869998755678715904", [2022])
     league: League = espnLeagueLoader.loadLeague()
 
     # Save league stats to an Excel sheet.
